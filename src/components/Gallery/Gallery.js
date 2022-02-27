786ba4c482e5ee04ab52c2be0922e47e98b94d50
vaibhav saini <vbsaini2004@gmail.com>
2022-02-27 18:43:24 +0530

    try

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./Gallery.css";
-
+import LazyLoad from "react-lazyload";
 function importAll(r) {
   let images = [];
   r.keys().map((item, index) => {
@@ -17,36 +17,38 @@ const images = importAll(
 export const Gallery = () => {
   const [count, setCount] = useState(0);
   let values = Object.values(images);
+  document.querySelector("body").style.overflowY = "scroll";
+  document.querySelector("nav").style.display = "flex";
   if (count === values.length - 1) {
-    document.querySelector("body").style.overflowY = "scroll";
-    document.querySelector("nav").style.display = "flex";
-    document
-      .querySelector(".gallery_header")
-      .classList.remove("loading_container");
-    document
-      .querySelector(".images_container")
-      .classList.remove("loading_img_container");
-    document.querySelector(".spinner").classList.add("dis_none");
+    // document
+    //   .querySelector(".gallery_header")
+    //   .classList.remove("loading_container");
+    // document
+    //   .querySelector(".images_container")
+    //   .classList.remove("loading_img_container");
+    // document.querySelector(".spinner").classList.add("dis_none");
   }
   return (
     <div>
-      <div className="gallery_header loading_container">
+      <div className="gallery_header ">
         <h2>GALLERY</h2>
       </div>
-      <div className="spinner">
+      <div className="spinner dis_none">
         <div className="loader"></div>
       </div>
-      <div className="images_container loading_img_container">
+      <div className="images_container ">
         {values.map((value) => (
           <div className="gallery_img_container" key={value}>
-            <img
-              className="gallery_img"
-              src={value}
-              alt=""
-              onLoad={() => {
-                setCount(count + 1);
-              }}
-            ></img>
+            <LazyLoad height={"100%"} offset={100}>
+              <img
+                className="gallery_img"
+                src={value}
+                alt=""
+                onLoad={() => {
+                  setCount(count + 1);
+                }}
+              ></img>
+            </LazyLoad>
           </div>
         ))}
       </div>
